bd19b64b4f7054d42664d848c513c6090366fce9
Lucas Barbosa Cesar de Oliveira <lucascoliveira819@gmail.com>
2022-09-09 13:52:57 -0400

    implemented home structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,32 @@
+import Image from "next/future/image";
 import { styled } from "../styles"
+import { HomeContainer, Product } from './../styles/pages/home'
 
-const Button = styled('button', {
-  backgroundColor: "$green300",
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
+import tshirt1 from '../assets/shirts/1.png'
+import tshirt2 from '../assets/shirts/2.png'
+import tshirt3 from '../assets/shirts/3.png'
 
-  span: {
-    fontWeight: 'bold'
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-})
 
 export default function Home() {
   return (
-    <Button>
-      <span>Teste</span>
-      Enviar
-    </Button>
+    <HomeContainer>
+
+      <Product>
+        <Image src={tshirt1} width={520} height={480} alt={""} />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={tshirt2} width={520} height={480} alt={""} />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+    </HomeContainer>
   )
 }
