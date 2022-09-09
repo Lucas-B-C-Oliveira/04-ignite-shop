4d8983502b53eec5947e41e9b9394fab53b6513c
Lucas Barbosa Cesar de Oliveira <lucascoliveira819@gmail.com>
2022-09-09 11:04:20 -0400

    change image to future/imagem

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,7 @@ import { AppProps } from "next/app"
 import { globalStyles } from "../styles/global"
 import logoImg from '../assets/logo.svg'
 import { Container, Header } from './../styles/pages/app';
-import Image from 'next/image'
-
+import Image from 'next/future/image'
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
